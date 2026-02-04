b3f02836d85336b3825c0facc8aace3aacb12abc
Farhan-Khan7 <fk5518744@gmail.com>
2026-02-04 16:54:07 +0530

    Operator problems

--- Day 1 of JavaScript/Script.js
+++ Day 1 of JavaScript/Script.js
@@ -1,7 +1,12 @@
-a = 10;
-b = 3;
+// a = 10;
+// b = 3;
 
-console.log(`Addition ${a+b}`)
-console.log(`subtraction ${a-b}`)
-console.log(`multiplacation ${a*b}`)
-console.log(`division ${a/b}`)
+// console.log(`Addition ${a+b}`)
+// console.log(`subtraction ${a-b}`)
+// console.log(`multiplacation ${a*b}`)
+// console.log(`division ${a/b}`)
+
+
+let x = 5;
+x += 3;
+console.log(x)
