c59deba4a714d1497989e495cef3cf901b883767
Farhan-Khan7 <fk5518744@gmail.com>
2026-04-19 12:20:55 +0530

    I have done

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
-var box = document.querySelector(".box");
-var increment = document.querySelector(".increment");
-var decrement = document.querySelector(".decrement");
-var h1 = document.querySelector("h1");  
 
-increment.addEventListener("click", function(){
-    h1.innerText = parseInt(h1.innerText) + 1;
-});
 
-decrement.addEventListener("click", function(){
-    h1.innerText = parseInt(h1.innerText) - 1;
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var box = document.querySelector(".box");
+// var increment = document.querySelector(".increment");
+// var decrement = document.querySelector(".decrement");
+// var h1 = document.querySelector("h1");  
+
+// increment.addEventListener("click", function(){
+//     h1.innerText = parseInt(h1.innerText) + 1;
+// });
+
+// decrement.addEventListener("click", function(){
+//     h1.innerText = parseInt(h1.innerText) - 1;
+// });
