617cbb3fb32567c901ca71db6f66b278a32e1134
Farhan-Khan7 <fk5518744@gmail.com>
2026-04-24 02:16:49 +0530

    I have done

--- script.js
+++ script.js
@@ -1,47 +1,6 @@
+let arr = ["apple", "mango", "banana", "lichi", "cherry"]
 
-let x = 5;
-let y = 3;
+console.log(arr.push("grapes"))
+console.log(arr.shift("Guawa"))
 
-x = x ^ y;
-y = x ^ y;
-x = x ^ y;
-
-console.log(x)
-console.log(y)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var box = document.querySelector(".box");
-// var increment = document.querySelector(".increment");
-// var decrement = document.querySelector(".decrement");
-// var h1 = document.querySelector("h1");  
-
-// increment.addEventListener("click", function(){
-//     h1.innerText = parseInt(h1.innerText) + 1;
-// });
-
-// decrement.addEventListener("click", function(){
-//     h1.innerText = parseInt(h1.innerText) - 1;
-// });
+console.log(arr)
