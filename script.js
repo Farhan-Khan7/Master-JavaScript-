6af5dc7516d73828039e9bf49de7dd48ea6de393
Farhan-Khan7 <fk5518744@gmail.com>
2026-04-27 01:31:14 +0530

    I have done global execution context or difference var let and const

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-let arr = ["apple", "mango", "banana", "lichi", "cherry"]
+console.log("value of x is", x);  // undefined
+var x = 10;
+console.log("value of x is", x);
 
-console.log(arr.push("grapes"))
-console.log(arr.shift("Guawa"))
 
-console.log(arr)
+
+
+
+
+
+
+
+
+// // Hint — structure kuch aisa hoga
+// function createCart() {
+//     let count = 0;
+    
+//     return {
+//         increment: function() {
+//             console.log(++count);
+//         },
+//         decrement: function() {
+//             console.log(--count);
+//         },
+//         reset: function() {
+//             console.log(count = 0);
+//         }
+//     }
+// }
+
+// let cart = createCart()
+
+// cart.increment()  // 1
+// cart.increment()  // 2
+// cart.increment()    // 3
+// cart.decrement()    // 2
+// cart.decrement()    // 1
+// cart.reset()    // 0
