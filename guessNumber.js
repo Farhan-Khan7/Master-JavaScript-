73da657ccf8f4d857de5c35aeab941701939b5b6
Farhan-Khan7 <fk5518744@gmail.com>
2026-05-23 19:21:15 +0530

    I have done some question

---
+++ guessNumber.js
@@ -0,0 +1,23 @@
+let prompt = require("prompt-sync")()
+
+function RandomNumberGenerat() {
+
+    let randomNumber = Math.floor(Math.random() * 100 + 1);
+
+    let attempts = 0;
+    let guessNumber;
+
+    do {
+        guessNumber = Number(prompt('Guess the Same Number = '))
+        attempts++;
+        if (guessNumber > randomNumber) console.log("Your Number is Too High");
+        else if (guessNumber < randomNumber) console.log("Your Number is Too Low");
+        else if (guessNumber === randomNumber) return `You Found it Exact Number in ${attempts} attempts`
+    } while (randomNumber !== guessNumber)
+
+}
+
+
+let result = (RandomNumberGenerat())
+console.log(result)
+
