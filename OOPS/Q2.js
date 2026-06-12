7875abcf3ecbbdcd6fffd316bc6db95ef58525d9
Farhan-Khan7 <fk5518744@gmail.com>
2026-06-13 01:35:47 +0530

    Finally clear prototype and prototypal inheritance

--- OOPS/Q2.js
+++ OOPS/Q2.js
@@ -1,8 +1,61 @@
-let obj = {
-    name : "Farhan",
-    greet : function(){
-        console.log("Hello " + this.name);
-    }
-}
+// console.log("Array ka prototype ka type ")
+// console.log(typeof(Array.__proto__));
 
-console.log(obj.name); // Output: Farhan
+// // esa likhne par sabme function mil raha hai kuch is tarike se "ƒ () { [native code] }"
+// console.log((Array.__proto__));
+
+// // Esaa likhne per sabka last prototype object yani null se pehle ki ek step mil rahi hai 
+// console.log((Array.__proto__.__proto__));
+
+// // Or esa likhne per Array ki methods mil rahi hai jo uske protytype me majod hai 
+// console.log((Array.prototype));
+
+// // To mera sawal ye hai ki prototype or .__proto__ alag alag tarike se kyu kaam kar raha hai .__proto__ or prototype kaa 
+// // output same hona chahiye naa 
+
+// console.log("\n\nObject ka prototype ka type")
+// console.log(typeof(Object.__proto__));
+// console.log((Object.__proto__));
+// console.log((Object.__proto__.__proto__));
+// console.log((Object.prototype));
+
+
+
+// console.log("\n\nFunction ka prototype ka type")
+// console.log(typeof(Function.__proto__));
+// console.log((Function.__proto__));
+// console.log((Function.__proto__.__proto__));
+// console.log((Function.prototype));
+
+
+
+// console.log("\n\nString ka prototype ka type")
+// console.log(typeof(String.__proto__));
+// console.log((String.__proto__));
+// console.log((String.__proto__.__proto__));
+// console.log((String.prototype));
+
+
+
+// console.log("\n\nNumber ka prototype ka type")
+// console.log(typeof(Number.__proto__));
+// console.log((Number.__proto__));
+// console.log((Number.__proto__.__proto__));
+// console.log((Number.prototype));
+
+
+
+// console.log("\n\nboolean ka prototype ka type")
+// console.log(typeof(Boolean.__proto__));
+// console.log((Boolean.__proto__));
+// console.log((Boolean.__proto__.__proto__));
+// console.log((Boolean.prototype));
+
+
+
+// console.log(typeof(Array.prototype));
+// console.log(typeof(Object.prototype));
+// console.log(typeof(Function.prototype));
+// console.log(typeof(String.prototype));
+// console.log(typeof(Number.prototype));
+// console.log(typeof(Boolean.prototype));
